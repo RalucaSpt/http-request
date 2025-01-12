ee1beecabece1b42e084355def438cf4271cbe0f
Raluca <spatarularisaraluca@gmail.com>
2025-01-12 02:40:11 +0200

    Handling HTTP Errors

--- 01-starting-project/src/app/places/available-places/available-places.component.ts
+++ 01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -18,13 +18,21 @@ export class AvailablePlacesComponent implements OnInit {
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
   isFetchingData = signal<boolean>(false);
+  error = signal<string | null>(null);
   ngOnInit(): void {
       this.isFetchingData.set(true);
       const subscription = this.httpClient.get<any>('http://localhost:3000/places').pipe(
-        map((resData) =>  resData.places)
+        map((resData) =>  resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('An error occurred while fetching data'));
+        })
       ).subscribe({
         next: (places) =>{
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetchingData.set(false);
